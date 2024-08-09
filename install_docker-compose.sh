e8f94bf89e469d19caf1b7afa2a9cf71d9a81065
azheno <wangzhendong.6776@foxmail.com>
2024-08-09 16:53:38 +0800

    V1.0.8

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -13,36 +13,49 @@ get_arch(){
 	echo "OS Version is : $arch" 
 }
 
+
+install_docker(){
+	if which docker >/dev/null; then
+		echo '检测到docker 已经安装，跳过安装步骤'
+	else 
+		echo -e "----------------------------------------------------"
+		echo -e "安装docker运行环境:"
+		tar -zxvf ./$arch/extendinstall.tar.gz
+		tar -xvf ./extendinstall/docker.tgz -C ./extendinstall
+		groupadd docker 
+		chmod 777 ./extendinstall/docker/* 
+		cp ./extendinstall/docker/* /usr/bin/
+		chmod +x ./extendinstall/*
+		cp ./extendinstall/docker.service ./extendinstall/docker.socket ./extendinstall/containerd.service /etc/systemd/system/
+		rm -rf ./extendinstall/docker
+	fi 
+	systemctl daemon-reload 
+	systemctl enable --now containerd.service
+	systemctl enable --now docker.socket
+	systemctl enable --now docker.service
+	docker -v 
+}
+
+install_docker_compose(){
+	
+	if which docker-compose >/dev/null; then
+		echo "检测到 Docker Compose 已安装，跳过安装步骤"
+	else
+		tar -zxvf ./$arch/extendinstall.tar.gz 
+		cp ./extendinstall/docker-compose /usr/bin/docker-compose
+		chmod -R 777 /usr/bin/docker-compose
+	fi
+	
+	/usr/bin/docker-compose -v
+}
+
+
+clean_cache(){
+	rm -rf ./extendinstall/
+}
+
 get_arch 
-
-if which docker >/dev/null; then
-  echo '检测到docker 已经安装，跳过安装步骤'
-else 
-  echo -e "----------------------------------------------------"
-  echo -e "安装docker运行环境:"
-  tar -zxvf ./$arch/extendinstall.tar.gz
-  tar -xvf ./extendinstall/docker.tgz -C ./extendinstall
-  groupadd docker 
-  chmod 777 ./extendinstall/docker/* 
-  cp ./extendinstall/docker/* /usr/bin/
-  chmod +x ./extendinstall/*
-  cp ./extendinstall/docker.service ./extendinstall/docker.socket ./extendinstall/containerd.service /etc/systemd/system/
-  rm -rf ./extendinstall/docker
-fi 
-systemctl daemon-reload 
-systemctl enable --now containerd.service
-systemctl enable --now docker.socket
-systemctl enable --now docker.service
-docker -v 
-if which docker-compose >/dev/null; then
-  echo "检测到 Docker Compose 已安装，跳过安装步骤"
-else
-  cp ./extendinstall/docker-compose /usr/bin/docker-compose
-  chmod -R 777 /usr/bin/docker-compose
-fi
-/usr/bin/docker-compose -v
-rm -rf ./extendinstall/
-
-
-
+install_docker
+install_docker_compose 
+clean_cache 
 
